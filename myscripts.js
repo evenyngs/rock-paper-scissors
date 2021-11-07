46777db7b8bcd80cda696c60868b038d7fb0091f
Tressa <thunterlearns@gmail.com>
2021-11-07 18:08:48 +0000

    finished function for player vs computer. need to have player input selection next

--- myscripts.js
+++ myscripts.js
@@ -1,1 +1,54 @@
-console.log("test")
+let r = "rock";
+let p = "paper";
+let s = "scissors";
+
+function computerPlay() {
+    //generate random number
+    let n = Math.random();
+
+    if (n < 0.34) {
+        n = r;
+    } else if (n <= 0.67) {
+        n = p;
+    } else {
+        n = s;
+    }
+    return n;
+}
+
+
+function playRound(playerSelection, computerSelection) {
+    console.log("Player: " + playerSelection)
+    console.log("Computer: " + computerSelection)
+    if (playerSelection == computerSelection) {
+        return ("Tie");
+    }
+    //player chooses rock
+    else if (playerSelection == r && computerSelection == s) {
+        return ("You win. Rock beats scissors.")
+    } else if (playerSelection == r && computerSelection == p) {
+        return ("You lose. Paper beats rock.")
+    }
+
+    //player chooses paper
+    else if (playerSelection == p && computerSelection == r) {
+        return ("You win. Paper beats rock.")
+    } else if (playerSelection == p && computerSelection == s) {
+        return ("You lose. Scissors beats paper.")
+    }
+    //player chooses scissors
+    else if (playerSelection == s && computerSelection == p) {
+        return ("You win. Scissors beats paper.")
+    } else if (playerSelection == s && computerSelection == r) {
+        return ("You lose. Rock beats scissors.")
+    }
+
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+
+//function game() {
+
+//}
